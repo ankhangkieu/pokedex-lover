1b83a27ebde3b87308a23c685ec8c46b85692086
ankhangkieu <ankhangkieu@gmail.com>
2017-01-29 23:28:35 -0800

    Initial Commit

---
+++ pokedex-lover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-lover
+//
+//  Created by Ak Kieu on 1/29/17.
+//  Copyright © 2017 Ak Kieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
